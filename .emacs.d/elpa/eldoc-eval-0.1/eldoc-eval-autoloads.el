589ea5c15e7a7c28da04b8dcd79af4faa18dc9b3
Bi Ge <seange727@gmail.com>
2015-09-01 09:24:05 -0400

    change to python environment

---
+++ .emacs.d/elpa/eldoc-eval-0.1/eldoc-eval-autoloads.el
@@ -0,0 +1,15 @@
+;;; eldoc-eval-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("eldoc-eval.el") (21895 15269 830129 180000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; eldoc-eval-autoloads.el ends here
---
+++ .emacs.d/elpa/pyvenv-1.7/pyvenv-pkg.el
@@ -0,0 +1,1 @@
+(define-package "pyvenv" "1.7" "Python virtual environment interface" 'nil)
